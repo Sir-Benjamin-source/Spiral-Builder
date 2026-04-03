984bc34fd7f06037e66cb5dce1a1dea3280cce0c
Sir Benjamin <s.benjaminfriend@gmail.com>
2026-04-03 22:35:00 +0000

    feat: add embody/__init__.py to make package importable

---
+++ embody/__init__.py
@@ -0,0 +1,10 @@
+"""
+Spiral-Builder embody package.
+
+This package provides the embodiment process for the Spiral ecosystem.
+"""
+
+from .embody import main as embody
+
+__version__ = "0.1.0"
+__all__ = ["embody"]
