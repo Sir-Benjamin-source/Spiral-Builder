1621eb0795bebc9c550fc22597565dcc78e7470d
Sir Benjamin <s.benjaminfriend@gmail.com>
2026-05-24 18:47:18 -0400

    feat(grokulator): Expose FormulaRegistry at package level

--- grokulator/__init__.py
+++ grokulator/__init__.py
@@ -11,6 +11,7 @@ Core focus:
 
 from .core.symbol_resolver import SymbolResolver
 from .core.discordance_handler import DiscordanceHandler
+from .core.formula_registry import FormulaRegistry
 from .data.symbolic_table import SymbolicTable
 from .utils.provenance import ProvenanceTracker
 
@@ -19,6 +20,7 @@ __version__ = "0.1.0"
 __all__ = [
     "SymbolResolver",
     "DiscordanceHandler",
+    "FormulaRegistry",
     "SymbolicTable",
     "ProvenanceTracker",
 ]
