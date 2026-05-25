9a3ae25f0ad0d207d79a96a6a425131714adcef9
Sir Benjamin <s.benjaminfriend@gmail.com>
2026-05-24 20:17:27 -0400

    docs(grokulator): Add grandmas_wisdom examples package init

---
+++ grokulator/examples/grandmas_wisdom/__init__.py
@@ -0,0 +1,5 @@
+# Grandma's Wisdom examples using the Grokulator
+
+# These examples demonstrate how Grandma's Wisdom concepts
+# (evidential assessment, bullshit meter, supporting claims, etc.)
+# can be expressed using Grokulator primitives.
